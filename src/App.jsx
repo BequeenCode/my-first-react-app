683199748338c19df3a7654691afdccf16e7c0f3
BequeenCode <girumbeimnet74@gmail.com>
2025-07-10 05:54:43 -0700

    update the features

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import { useEffect, useState } from 'react';
+
 // Card Component
-const Card = () => {
+const Card = ({ title }) => {
+  const [count, setCount] = useState(0);
+  const [hasLiked, setHasLiked] = useState(false);
+
+  useEffect(() => {
+    console.log(`${title} has been liked: ${hasLiked}`);
+  }, [hasLiked]);
+
   return (
-    <div>
-      <h2>Card Component</h2>
+    <div className="card" onClick={() => setCount(count + 1)}>
+      <h2>{title} - {count}</h2>
+      <button onClick={(e) => {
+        e.stopPropagation(); // prevent parent click
+        setHasLiked(!hasLiked);
+      }}>
+        {hasLiked ? '❤️ ' : '👍 '}
+      </button>
     </div>
   );
 };
@@ -10,15 +25,12 @@ const Card = () => {
 // App Component
 const App = () => {
   return (
-    <div>
-      <h2>Functional Arrow Component</h2>
-
-      <Card />
-      <Card />
-      <Card />
+    <div className="card-container">
+      <Card title="Star Wars" />
+      <Card title="Avatar" />
+      <Card title="The Lion King" />
     </div>
   );
 };
 
 export default App;
-
