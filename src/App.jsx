e6fe52c44a07e541dabb6e90e23c617a3a6bdd80
BequeenCode <girumbeimnet74@gmail.com>
2025-07-16 06:52:53 -0700

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,13 @@
-import { useEffect, useState } from 'react';
+import React from "react";
 
-// Card Component
-const Card = ({ title }) => {
-  const [count, setCount] = useState(0);
-  const [hasLiked, setHasLiked] = useState(false);
-
-  useEffect(() => {
-    console.log(`${title} has been liked: ${hasLiked}`);
-  }, [hasLiked]);
-  
-
+const App = () =>{
   return (
-    <div className="card" onClick={() => setCount(count + 1)}>
-      <h2>{title} - {count}</h2>
-      <button onClick={(e) => {
-        e.stopPropagation(); // prevent parent click
-        setHasLiked(!hasLiked);
-      }}>
-        {hasLiked ? '❤️ ' : '👍 '}
-      </button>
+    <div>
+      <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
     </div>
-  );
-};
+  )
+}
 
-// App Component
-const App = () => {
-  return (
-    <div className="card-container">
-      <Card title="Star Wars" />
-      <Card title="Avatar" />
-      <Card title="The Lion King" />
-    </div>
-  );
-};
-
-export default App;
+export default App
