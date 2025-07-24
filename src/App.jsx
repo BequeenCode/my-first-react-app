5f5a6be1c85d50d34ee279f53f3643ce2412446d
BequeenCode <girumbeimnet74@gmail.com>
2025-07-24 00:49:22 -0700

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import Search from "./components/search";
 
-const App = () =>{
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState('I AM BATMAN');
+searchTerm ='NEW SEARCH TERM.'
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-    </div>
-  )
-}
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> you'll enjoy the Hassle
+          </h1>
+        </header>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>
+    </main>
+  );
+};
 
-export default App
+export default App;
